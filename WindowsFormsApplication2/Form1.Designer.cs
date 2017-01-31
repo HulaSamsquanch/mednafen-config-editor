53eae092929851223d8456c8d7f65ee99e0f91b7
HulaSamsquanch <samsquanchhunter14@gmail.com>
2017-01-31 16:34:23 -0600

    aasdasdad

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(5, 31);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(220, 225);
+            this.listBox2.TabIndex = 0;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(5, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(220, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Search";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(231, 233);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "set";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(231, 207);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(145, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(231, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(145, 138);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(388, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Mednafen Config Editor";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+
+        String[] configFile;
+        Dictionary<string, string> dict = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadStuff();
+
+        }
+
+        private void ResetTextBox()
+        {
+            this.textBox2.ResetText();
+        }
+
+        private void LoadStuff()
+        {
+            textBox2.Text = "Loading...";
+            listBox2.ClearSelected();
+            dict.Clear();
+
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                /* run your code here */
+                configFile = System.IO.File.ReadAllLines(@"./mednafen-09x.cfg");
+                foreach (String element in configFile)
+                {
+                    if (!element.StartsWith(";"))
+                    {
+                        //a comment
+
+                        String[] splitElement = element.Split(' ');
+
+                        if (splitElement.Length == 2)
+                        {
+                            try
+                            {
+                                Console.Write(splitElement[0] + " :: " + splitElement[1]);
+                                dict.Add(splitElement[0], splitElement[1]);
+                            }
+                            catch (Exception)
+                            {
+                                throw;
+                            }
+                        }
+                        
+
+                    }
+                    
+                }
+                this.ResetTextBox();
+            }).Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string va = listBox2.GetItemText(listBox2.SelectedItem);
+
+
+            if (dict.ContainsKey(va))
+            {
+                textBox3.Text = dict[va];
+                // "tester" key exists and contains "testing" value
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+            listBox2.Items.AddRange(dict.Keys.Where(X => X.Contains(textBox2.Text)).ToArray());
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var t = 0;
+            foreach (String s in configFile) {
+                t++;
+                if (s.StartsWith(listBox2.GetItemText(listBox2.SelectedItem))) {
+                    break;
+                }
+            }
+            using (StreamWriter writer = new StreamWriter(@"./mednafen-09x.cfg.backup"))
+            {
+                foreach (String s in configFile) {
+                    writer.Write(s + "\r\n");
+                }
+            }
+            configFile[t-1] = listBox2.GetItemText(listBox2.SelectedItem) + " " + textBox3.Text;
+            using (StreamWriter writer = new StreamWriter(@"./mednafen-09x.cfg"))
+            {
+                foreach (String s in configFile)
+                {
+                    writer.Write(s + "\r\n");
+                }
+            }
+
+            LoadStuff();
+
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.Equals(Keys.Enter))
+                button2.PerformClick();
+        }
+    }
+}
